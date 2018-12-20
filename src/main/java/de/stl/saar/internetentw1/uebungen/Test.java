bb6ebef2b16ac006c58980a9b0b05ba7fbb13320
xNoFear <kevin-engelhardt@gmx.de>
2018-12-20 23:51:59 +0100

    Ein Haufen geknaupt

---
+++ src/main/java/de/stl/saar/internetentw1/uebungen/Test.java
@@ -0,0 +1,9 @@
+package de.stl.saar.internetentw1.uebungen;
+
+public class Test {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Hat geklappt, du Pisser");
+    }
+}
